6311a6465b18105b2440d3b5b4e53642bdb5ec48
世元 李 <1498045907@qq.com>
2021-12-10 16:19:43 +0800

    init

---
+++ Program.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import http.Server;
+
+public class Program
+{
+	static public void main(String []argv)
+	{
+		Server HttpSv=new Server();
+		HttpSv.Start();
+	}
+}
---
+++ http/Server.java
@@ -0,0 +1,29 @@
+package Server;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Server {
+	public Server()
+	{
+		HttpServer server = HttpServer.create(new InetSocketAddress(8001), 0);
+        server.createContext("/index.html", new Index());
+        server.start();
+	}
+
+    static  class Index implements HttpHandler{
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "hello world";
+            exchange.sendResponseHeaders(200, 0);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
